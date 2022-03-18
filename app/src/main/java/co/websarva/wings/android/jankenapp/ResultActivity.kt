323b7e339655662411b7a6a9d2e8e12b31ad2382
kouhei <grimpoteuthistako@gmail.com>
2022-03-18 23:52:58 +0900

    MainActivity：Intentにじゃんけんの画像のIDを持たせた ResultActivity：Intentから画像のID取得し、whenで場合分け

--- app/src/main/java/co/websarva/wings/android/jankenapp/ResultActivity.kt
+++ app/src/main/java/co/websarva/wings/android/jankenapp/ResultActivity.kt
@@ -2,10 +2,21 @@ package co.websarva.wings.android.jankenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.websarva.wings.android.jankenapp.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_result)
+        val id = intent.getIntExtra("MY_HAND",0)
+
+        when(id){
+            R.id.gu -> binding.myHandImage.setImageResource(R.drawable.gu)
+            R.id.choki -> binding.myHandImage.setImageResource(R.drawable.choki)
+            R.id.pa-> binding.myHandImage.setImageResource(R.drawable.pa)
+        }
     }
 }
