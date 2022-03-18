de65f41ce3005631557d78031b96ab20d315c17e
kouhei <grimpoteuthistako@gmail.com>
2022-03-18 23:36:33 +0900

    MainActivity.ktのじゃんけんの手にリスナーを設定した。

--- app/src/main/java/co/websarva/wings/android/jankenapp/MainActivity.kt
+++ app/src/main/java/co/websarva/wings/android/jankenapp/MainActivity.kt
@@ -1,12 +1,29 @@
 package co.websarva.wings.android.jankenapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import co.websarva.wings.android.jankenapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gu.setOnClickListener{onJankenButtonTapped(it)}
+        binding.choki.setOnClickListener{onJankenButtonTapped(it)}
+        binding.pa.setOnClickListener { onJankenButtonTapped(it)}
     }
+
+    fun onJankenButtonTapped(view: View?){
+        val intent = Intent(this,ResultActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
