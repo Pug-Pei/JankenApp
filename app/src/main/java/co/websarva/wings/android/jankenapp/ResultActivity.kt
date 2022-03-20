381eba6c93e7f2341d12b4b913a9017b4f5b2b03
kouhei <grimpoteuthistako@gmail.com>
2022-03-20 21:06:25 +0900

    ResultActivity：じゃんけんの判定ロジックを追加

--- app/src/main/java/co/websarva/wings/android/jankenapp/ResultActivity.kt
+++ app/src/main/java/co/websarva/wings/android/jankenapp/ResultActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import co.websarva.wings.android.jankenapp.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    val gu = 0
+    val choki = 1
+    val pa = 2
     private lateinit var binding:ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,10 +16,35 @@ class ResultActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_result)
         val id = intent.getIntExtra("MY_HAND",0)
 
-        when(id){
-            R.id.gu -> binding.myHandImage.setImageResource(R.drawable.gu)
-            R.id.choki -> binding.myHandImage.setImageResource(R.drawable.choki)
-            R.id.pa-> binding.myHandImage.setImageResource(R.drawable.pa)
+        val myHand: Int
+        myHand = when(id){
+            R.id.gu -> {
+                binding.myHandImage.setImageResource(R.drawable.gu)
+                gu
+            }
+            R.id.choki -> {
+                binding.myHandImage.setImageResource(R.drawable.choki)
+                choki
+            }
+            R.id.pa-> {binding.myHandImage.setImageResource(R.drawable.pa)
+            pa
+            }
+            else -> gu
+        }
+        //　コンピューターの手を決める
+        val comHand = (Math.random() * 3).toInt()
+        when(comHand) {
+            gu -> binding.comHandImage.setImageResource(R.drawable.com_gu)
+            choki -> binding.myHandImage.setImageResource(R.drawable.com_choki)
+            pa -> binding.myHandImage.setImageResource(R.drawable.com_pa)
+        }
+
+        //勝敗の判定をする
+        val gameResult = (comHand - myHand + 3) % 3
+        when(gameResult){
+            0 -> binding.resultLabel.setText(R.string.result_draw) //引き分け
+            1 -> binding.resultLabel.setText(R.string.result_win) //あなたの勝ち
+            2 -> binding.resultLabel.setText(R.string.result_lose) //あなたの負け
         }
     }
 }
